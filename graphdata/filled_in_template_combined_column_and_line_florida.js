b7f9b7474bcd4591e10c49c5af7a8126c5e9ec12
Bruce Dockeray <bruce@dockeray.co.uk>
2020-07-09 23:09:27 +0100

    florida

---
+++ graphdata/filled_in_template_combined_column_and_line_florida.js
@@ -0,0 +1,32 @@
+
+  
+// https://jsfiddle.net/BlackLabel/sxuhq3pn/  
+//$.get('graphdata/graph_data_UKGM_cu.csv', function(csv) {
+
+$(document).ready(function() {
+
+  $.get('graphdata/graph_data_florida_combined.csv', function(data) {
+
+    $('#containerChartFlorida').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Florida)'
+    },
+    });
+  });
+
+});
+
