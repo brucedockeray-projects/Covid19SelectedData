a83bf2480ed9c59dfff59faecd107ccfe68f8211
Bruce Dockeray <bruce@dockeray.co.uk>
2020-07-10 08:34:29 +0100

    new data

--- graphdata/filled_in_template_combined_column_and_line_selected_states.js
+++ graphdata/filled_in_template_combined_column_and_line_selected_states.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
     });
   });
 
+
 $.get('graphdata/graph_data_california_combined.csv', function(data) {
 
     $('#containerChartCalifornia').highcharts({
@@ -51,9 +52,101 @@ $.get('graphdata/graph_data_california_combined.csv', function(data) {
     });
   });
 
+$.get('graphdata/graph_data_texas_combined.csv', function(data) {
+
+    $('#containerChartTexas').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Texas)'
+    },
+    });
+  });
+
+$.get('graphdata/graph_data_georgia_combined.csv', function(data) {
+
+    $('#containerChartGeorgia').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Georgia)'
+    },
+    });
+  });
+
+$.get('graphdata/graph_data_nevada_combined.csv', function(data) {
+
+    $('#containerChartNevada').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Nevada)'
+    },
+    });
+  });
+
+$.get('graphdata/graph_data_alabama_combined.csv', function(data) {
+
+    $('#containerChartAlabama').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Alabama)'
+    },
+    });
+  });
 
 
 
 // this bit at the end to close off the document ready function
+
 });
 
