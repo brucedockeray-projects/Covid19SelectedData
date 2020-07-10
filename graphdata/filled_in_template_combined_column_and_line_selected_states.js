9569d8ddfcc79d779bd79f1d8a07fc8ad136788b
Bruce Dockeray <bruce@dockeray.co.uk>
2020-07-10 07:29:10 +0100

    adding further us states combined data

---
+++ graphdata/filled_in_template_combined_column_and_line_selected_states.js
@@ -0,0 +1,59 @@
+
+  
+// https://jsfiddle.net/BlackLabel/sxuhq3pn/  
+//$.get('graphdata/graph_data_UKGM_cu.csv', function(csv) {
+
+$(document).ready(function() {
+
+  $.get('graphdata/graph_data_florida_combined.csv', function(data) {
+
+    $('#containerChartFlorida').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Florida)'
+    },
+    });
+  });
+
+$.get('graphdata/graph_data_california_combined.csv', function(data) {
+
+    $('#containerChartCalifornia').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (California)'
+    },
+    });
+  });
+
+
+
+
+// this bit at the end to close off the document ready function
+});
+
