10ab516277b6e267b4dfe5c6318bb8909751778b
Bruce Dockeray <bruce@dockeray.co.uk>
2020-09-03 08:21:49 +0100

    new data

--- graphdata/filled_in_template_combined_column_and_line_selected_states.js
+++ graphdata/filled_in_template_combined_column_and_line_selected_states.js
@@ -144,6 +144,29 @@ $.get('graphdata/graph_data_alabama_combined.csv', function(data) {
     });
   });
 
+$.get('graphdata/graph_data_louisiana_combined.csv', function(data) {
+
+    $('#containerChartLouisiana').highcharts({
+      chart: {
+        type: "column",
+        color: "#00FF00"
+      },
+      
+
+      data: {
+        csv: data,
+        switchRowsAndColumns: true,
+        complete: function(options) {
+          options.series[1].type = 'line';
+          //options.series[3].type = 'line';
+        }
+      },
+      title: {
+      text: 'Covid New Cases (Louisiana)'
+    },
+    });
+  });
+
 
 
 // this bit at the end to close off the document ready function
